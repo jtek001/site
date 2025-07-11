49e014e9c137c266a0c4f5c500434daf1215b965
jtek001 <jurandir.prestes@gmail.com>
2025-07-10 21:49:48 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,35 @@
+<?php require_once 'track_visitor.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jtek Info - Soluçes de TI para sua Empresa</title>
+    <meta name="description" content="Da infraestrutura de redes ao suporte técnico especializado, garantimos a tecnologia que sua empresa precisa para crescer.">
+       <link rel="icon" type="image/x-icon" href="img/android-icon-48x48.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@700;900&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+    <header class="main-header">
+        <div class="container">
+            <a href="/index.php" class="logo">
+                <img src="/img/logo.png" height="50" alt="Jtek Info"> 
+            </a>
+
+            <nav class="main-nav">
+                <a href="/index.php">Incio</a>
+                <a href="/servicos.php">Serviços</a>
+                <a href="/sobre.php">Sobre</a>
+                <a href="/parceiros.php">Parceiros</a>
+                <a href="/gdoor" class="btn-gdoor">Gdoor</a> 
+                <a href="/clientes.php">Clientes</a>
+                <a href="https://blog.jtekinfo.com.br/" target="_blank">Blog</a>                
+                <a href="/contato.php" class="btn-contact">Contato</a>
+            </nav>
+        </div>
+    </header>
