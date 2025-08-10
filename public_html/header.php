492992e4ad3cc2c11d400ef10f76ea8f8a75592b
Jurandir Prestes <jurandir.prestes@gmail.com>
2025-08-10 18:45:49 -0300

    Create header.php

---
+++ public_html/header.php
@@ -0,0 +1,35 @@
+<?php require_once 'track_visitor.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jtek Info - Soluçes de TI para sua Empresa</title>
+    <meta name="description" content="Da infraestrutura de redes ao suporte técnico especializado, garantimos a tecnologia que sua empresa precisa para crescer.">
+       <link rel="icon" type="image/x-icon" href="img/android-icon-48x48.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@700;900&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+    <header class="main-header">
+        <div class="container">
+            <a href="/" class="logo">
+                <img src="/img/logo.png" height="50" alt="Jtek Info"> 
+            </a>
+
+            <nav class="main-nav">
+                <a href="/">Início</a>
+                <a href="/servicos">Serviços</a>
+                <a href="/sobre">Sobre</a>
+                <a href="/projetos">Projetos</a>
+                <a href="/parceiros">Parceiros</a>
+                <a href="/gdoor" class="btn-gdoor">Gdoor</a> 
+                <a href="/clientes">Clientes</a>
+                <a href="/contato" class="btn-contact">Contato</a>
+            </nav>
+        </div>
+    </header>
